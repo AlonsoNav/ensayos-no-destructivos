726ee9340b984cc891be829a5891f7d09ae948da
johnS31 <jostsace@estudiantec.cr>
2025-04-15 15:48:40 -0600

    se rota el carrusel con las flechas del teclado

--- Assets/onSelectEntered.cs
+++ Assets/onSelectEntered.cs
@@ -6,23 +6,22 @@ public class MenuOption : MonoBehaviour
 {
     public string optionName;
 
-    private void OnEnable()
+    void OnEnable()
     {
         var interactable = GetComponent<XRBaseInteractable>();
         if (interactable != null)
             interactable.selectEntered.AddListener(OnSelect);
     }
 
-    private void OnDisable()
+    void OnDisable()
     {
         var interactable = GetComponent<XRBaseInteractable>();
         if (interactable != null)
             interactable.selectEntered.RemoveListener(OnSelect);
     }
 
-    private void OnSelect(SelectEnterEventArgs args)
+    void OnSelect(SelectEnterEventArgs args)
     {
         Debug.Log($"Seleccionaste la opción: {optionName}");
-        // Aquí puedes cargar una escena, activar UI, etc.
     }
 }
