ceb959c6fba220efa28868afb3176fe05eb0f6a9
Carlos Venegas <cvenegasm1043@gmail.com>
2025-04-28 21:28:16 -0600

    Func. Primer Herramienta

--- Assets/Resources/TextoAlActivar3Sockets.cs
+++ Assets/Resources/TextoAlActivar3Sockets.cs
@@ -7,10 +7,17 @@ public class TextoAlActivarTresCirculos : MonoBehaviour
     public SocketSpriteColorYNumero circle1;
     public SocketSpriteColorYNumero circle2;
     public SocketSpriteColorYNumero circle3;
+    public SocketSpriteColorYNumero circle4;
+    public SocketSpriteColorYNumero circle5;
+    public SocketSpriteColorYNumero circle6;
+    public SocketSpriteColorYNumero circle7;
+    public SocketSpriteColorYNumero circle8;
+    public SocketSpriteColorYNumero circle9;
+    public SocketSpriteColorYNumero circle10;
 
     [Header("Texto")]
     public TextMeshPro textoCambiar;
-    public string textoCuandoActivos = "¡Los 3 círculos están activos!";
+    public string textoCuandoActivos = "¡Los 10 círculos están activos!";
 
     private void Update()
     {
@@ -19,7 +26,10 @@ public class TextoAlActivarTresCirculos : MonoBehaviour
 
     private void VerificarCondicion()
     {
-        if (circle1.estaActivado && circle2.estaActivado && circle3.estaActivado)
+        if (circle1.estaActivado && circle2.estaActivado && circle3.estaActivado && 
+            circle4.estaActivado && circle5.estaActivado && circle6.estaActivado &&
+            circle7.estaActivado && circle8.estaActivado && circle9.estaActivado &&
+            circle10.estaActivado)
         {
             textoCambiar.text = textoCuandoActivos;
         }
