ed116e2c50b0a881fe5fd4c1aad91f8d2c849c6b
Julianlopm <124851329+Julianlopmm@users.noreply.github.com>
2025-05-13 14:16:24 -0600

    actualizacion script pared

--- Assets/WallLightTrigger.cs
+++ Assets/WallLightTrigger.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
+
 
 public class WallLightTrigger : MonoBehaviour
 {
@@ -7,6 +9,7 @@ public class WallLightTrigger : MonoBehaviour
     public Color emissionColor = Color.cyan;
     private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable;
     private Material wallMaterial;
+    private bool isInteracting = false; // Para controlar si se está interactuando
 
     void Awake()
     {
@@ -18,12 +21,35 @@ public class WallLightTrigger : MonoBehaviour
 
     void OnGrab(SelectEnterEventArgs args)
     {
-        wallMaterial.EnableKeyword("_EMISSION");
-        wallMaterial.SetColor("_EmissionColor", emissionColor);
+        // Iniciar la corutina para iluminar la pared brevemente
+        if (!isInteracting)
+        {
+            StartCoroutine(ActivateWallLight());
+        }
     }
 
     void OnRelease(SelectExitEventArgs args)
     {
+        // Apagar la luz inmediatamente cuando se suelta
+        StopAllCoroutines();
         wallMaterial.SetColor("_EmissionColor", Color.black);
+    }
+
+    // Corutina que controla el parpadeo de la luz
+    private IEnumerator ActivateWallLight()
+    {
+        isInteracting = true;
+        
+        // Activar la emisión
+        wallMaterial.EnableKeyword("_EMISSION");
+        wallMaterial.SetColor("_EmissionColor", emissionColor);
+
+        // Esperar un segundo (puedes ajustar el tiempo)
+        yield return new WaitForSeconds(1f);
+
+        // Apagar la emisión después del tiempo de espera
+        wallMaterial.SetColor("_EmissionColor", Color.black);
+        
+        isInteracting = false;
     }
 }
